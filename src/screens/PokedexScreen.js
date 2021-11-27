e1267627427ac88928996b7e906014bd6ecb7d48
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-11-27 11:55:03 -0500

    feat: Configuración de React Navigation

---
+++ src/screens/PokedexScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+export default function PokedexScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Pokedex</Text>
+    </SafeAreaView>
+  );
+}
