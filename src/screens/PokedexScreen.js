b31e76cd5bd931cec6443b18a8eb7c41aa143398
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-11-30 12:43:31 -0500

    feat: Consumo de APIs con fetch: PokeApi

--- src/screens/PokedexScreen.js
+++ src/screens/PokedexScreen.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { SafeAreaView, Text } from 'react-native';
+import {getPokemonApi} from '../api/pokemon';
 
 export default function PokedexScreen() {
+  useEffect(() => {
+    (async () => {
+      await loadPokemons();
+    }) ()
+  }, []);
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonApi();
+      console.log(response);
+    } catch (error) {
+      console.error("error");
+    }
+  };
+
   return (
     <SafeAreaView>
       <Text>Pokedex</Text>
