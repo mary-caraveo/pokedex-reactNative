e0233fb8c2a0442efc27b749da307bd415904168
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-12-01 13:18:11 -0500

    feat: Clasificación por colores y utilidades de Lodash

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { StyleSheet, View, Text, Image, TouchableWithoutFeedback } from 'react-native';
+import getColorByPokemonType from '../utils/getColorByPokemonType';
+import { capitalize} from 'lodash'
 
 export default function PokemonCard(props) {
   const { pokemon } = props;
+  const pokemonColor = getColorByPokemonType(pokemon.type);
+  const bgStyles = { backgroundColor: pokemonColor, ...styles.bgStyles };
+
   const goToPokemon = () => {
     console.log(`Ir al pokemon: ${pokemon.name}`);
   };
@@ -10,13 +15,15 @@ export default function PokemonCard(props) {
   return (
     <TouchableWithoutFeedback onPress={goToPokemon}>
       <View style={styles.card}>
-      <View style={styles.spacing}>
-      <View style={styles.bgStyles}>
-      <Text style={styles.number}>#{`${pokemon.order}`.padStart(3,0)}</Text>
-        <Text style={styles.name}>{pokemon.name}</Text>
-        <Image source={{uri: pokemon.image }} style={styles.image} />
-      </View>
-      </View>
+        <View style={styles.spacing}>
+          <View style={bgStyles}>
+            <Text style={styles.number}>
+              #{`${pokemon.order}`.padStart(3, 0)}
+            </Text>
+            <Text style={styles.name}>{capitalize(pokemon.name)}</Text>
+            <Image source={{ uri: pokemon.image }} style={styles.image} />
+          </View>
+        </View>
       </View>
     </TouchableWithoutFeedback>
   );
@@ -32,7 +39,9 @@ const styles = StyleSheet.create({
     padding: 5,
   },
   bgStyles: {
-    backgroundColor: "grey",
+    flex: 1,
+    borderRadius: 15,
+    padding: 10,
   },
   number: {
     position: "absolute",
