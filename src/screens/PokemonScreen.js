2e3e9aa9ea1999dfbcd176b66657c1feab42fb4b
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-12-02 11:46:10 -0500

    feat: Consultando la API de detalles del Pokemon

--- src/screens/PokemonScreen.js
+++ src/screens/PokemonScreen.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
+import {getPokemonDetailsApi} from '../api/pokemon';
 
 export default function PokemonScreen(props) {
-  const { navigation, route } = props;
+  const {
+    navigation,
+    route: { params },
+  } = props;
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await getPokemonDetailsApi(params.id);
+        setPokemon(response);
+      } catch (error) {
+        navigation.goBack();
+      }
+    })();
+  }, [params]);
+
+  if(!pokemon) return null;
 
   return (
     <View>
       <Text>Pokemon</Text>
+      <Text>{pokemon.name}</Text>
     </View>
   );
 }
