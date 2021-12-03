3953af593540688cf4a0fc4b52493ce0fce828ee
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-12-03 10:28:11 -0500

    feat: Maquetación del Pokemon: estadísticas

--- src/screens/PokemonScreen.js
+++ src/screens/PokemonScreen.js
@@ -3,6 +3,7 @@ import { ScrollView } from 'react-native';
 import { getPokemonDetailsApi } from '../api/pokemon';
 import Header from '../components/pokemon/Header';
 import Type from '../components/pokemon/Type';
+import Stats from '../components/pokemon/Stats';
 
 export default function PokemonScreen(props) {
   const {
@@ -33,6 +34,7 @@ export default function PokemonScreen(props) {
         type={pokemon.types[0].type.name}
       />
       <Type types={pokemon.types} />
+      <Stats stats={pokemon.stats} />
     </ScrollView>
   );
 }
