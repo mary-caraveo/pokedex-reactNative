21c793235389ef6fc30f99182a6d4bb31ba52509
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-12-04 10:56:50 -0500

    feat: Iniciando sesión

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text, TextInput, Button, Keyboard } from 'react-native';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
+import { user, userDetails } from '../../utils/userDB';
 
 export default function LoginForm() {
+  const [error, setError] = useState("");
+
   const formik = useFormik({
     initialValues: initialValues(),
     validationSchema: Yup.object(validationSchema()),
     validateOnChange: false,
     onSubmit: (formValue) => {
-      console.log("Formulario enviado");
-      console.log(formValue);
+      setError("");
+      const { username, password } = formValue;
+
+      if (username != user.username || password !== user.password) {
+        setError("El usuario o la contraseña no son correctos");
+      } else {
+        console.log("Login correcto");
+      }
     },
   });
 
@@ -35,6 +44,7 @@ export default function LoginForm() {
       <Button title="Ingresar" onPress={formik.handleSubmit} />
       <Text style={styles.error}>{formik.errors.username}</Text>
       <Text style={styles.error}>{formik.errors.password}</Text>
+      <Text style={styles.error}>{error}</Text>
     </View>
   );
 }
