9b8612486eec391be932618a752937c3af07188a
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-12-07 12:15:21 -0500

    feat: Lista de Pokémon favoritos

--- src/screens/FavoriteScreen.js
+++ src/screens/FavoriteScreen.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import { SafeAreaView, Text, Button } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { Text } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
 import { getPokemonFavoriteApi } from '../api/favorite';
+import { getPokemonDetailsApi } from '../api/pokemon';
+import useAuth from '../hooks/useAuth';
+import PokemonList from '../components/PokemonList';
 
 export default function FavoriteScreen() {
-  const checkFavorites = async () => {
-    const response = await getPokemonFavoriteApi();
-  }
+  const [pokemons, setPokemons] = useState([]);
+  const { auth } = useAuth();
 
-  return (
-    <SafeAreaView>
-      <Text>Favorite</Text>
-      <Button title="Obtener favoritos" onPress={checkFavorites} />
-    </SafeAreaView>
+  useFocusEffect(
+    useCallback(() => {
+      if (auth) {
+        (async () => {
+          const response = await getPokemonFavoriteApi();
+          const pokemonsArray = [];
+
+          for await (const id of response) {
+            const pokemonDetails = await getPokemonDetailsApi(id);
+
+            pokemonsArray.push({
+              id: pokemonDetails.id,
+              name: pokemonDetails.name,
+              type: pokemonDetails.types[0].type.name,
+              order: pokemonDetails.order,
+              image:
+                pokemonDetails.sprites.other["official-artwork"].front_default,
+            });
+          }
+          setPokemons(pokemonsArray);
+        })();
+      }
+    }, [auth])
+  );
+
+  return !auth ? (
+    <Text>Usuario no logueado</Text>
+  ) : (
+    <PokemonList pokemons={pokemons} />
   );
 }
 
