a321f80ee01a41a787d0b451839d6796e02aa009
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2022-07-25 17:41:50 -0500

    feat: change of pill style

--- src/components/pokemon/Type.js
+++ src/components/pokemon/Type.js
@@ -1,39 +1,32 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { View, Text } from 'native-base';
 import { map, capitalize } from 'lodash';
 import getColorByPokemonType from '../../utils/getColorByPokemonType';
 
-export default function Type(props) {
+const Type = (props) => {
   const { types } = props;
 
   return (
-    <View style={styles.content}>
+    <View
+      marginTop="4"
+      flexDirection="row"
+      alignItems="center"
+      justifyContent="center">
       {map(types, (item, index) => (
         <View
-          key={index}
-          style={{
-            ...styles.pill,
-            backgroundColor: getColorByPokemonType(item.type.name),
-          }}
-        >
-          <Text>{capitalize(item.type.name)}</Text>
+          borderRadius="20"
+          paddingX="35"
+          paddingY="2"
+          marginX="4"
+          backgroundColor={getColorByPokemonType(item.type.name)}
+          key={index}>
+          <Text color="white" bold={true}>
+            {capitalize(item.type.name)}
+          </Text>
         </View>
       ))}
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  content: {
-    marginTop: 20,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  pill: {
-    paddingHorizontal: 30,
-    paddingVertical: 5,
-    borderRadius: 20,
-    marginHorizontal: 10,
-  },
-});
+export default Type;
