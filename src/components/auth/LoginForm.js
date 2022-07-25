0b905aa758e0224c433dd7fb64c0cc21be97c77c
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2022-07-25 15:44:39 -0500

    feat: add the Keyboard to the log in form

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import { Image } from 'react-native';
+import { Platform, Image } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
+import { Box } from 'native-base';
 import {
+  KeyboardAvoidingView,
   View,
   Text,
   Input,
@@ -16,6 +18,7 @@ import { user, userDetails } from '../../utils/userDB';
 import useAuth from '../../hooks/useAuth';
 
 const LoginForm = () => {
+  const behavior = Platform.OS === 'ios' ? 'padding' : 'height';
   const [show, setShow] = useState(false);
   const [error, setError] = useState('');
   const { login } = useAuth();
@@ -37,87 +40,96 @@ const LoginForm = () => {
   });
 
   return (
-    <View backgroundColor="white" height="100%" paddingTop={6}>
-      <Image
-        source={require('../../assets/account.png')}
-        resizeMode="cover"
-        style={{ width: 400, height: 200 }}
-      />
-
-      <View paddingX="25">
-        <Text fontSize="26" fontWeight="bold" marginTop="30" marginBottom="8">
-          Iniciar sesión
-        </Text>
-        <Stack space={4} marginBottom="10">
-          <FormControl isInvalid={!!formik.errors.username}>
-            <Input
-              variant="underlined"
-              placeholder="Nombre de usuario"
-              fontSize="14"
-              autoCapitalize="none"
-              value={formik.values.username}
-              onChangeText={(text) => formik.setFieldValue('username', text)}
-              InputLeftElement={
-                <Icon
-                  as={MaterialIcons}
-                  name="person"
-                  size="md"
-                  mx="3"
-                  color="muted.400"
-                />
-              }
-            />
-            <FormControl.ErrorMessage>
-              {formik.errors.username}
-            </FormControl.ErrorMessage>
-          </FormControl>
-          <FormControl isInvalid={!!formik.errors.password}>
-            <Input
-              variant="underlined"
-              placeholder="Contraseña"
-              fontSize="14"
-              autoCapitalize="none"
-              secureTextEntry={true}
-              value={formik.values.password}
-              onChangeText={(text) => formik.setFieldValue('password', text)}
-              InputLeftElement={
-                <Icon
-                  as={MaterialIcons}
-                  name="lock"
-                  size="md"
-                  mx="3"
-                  color="muted.400"
-                />
-              }
-              type={show ? 'text' : 'password'}
-              InputRightElement={
-                <Icon
-                  as={MaterialIcons}
-                  name={show ? 'visibility' : 'visibility-off'}
-                  size={5}
-                  mr="2"
-                  color="muted.400"
-                  onPress={() => setShow(!show)}
-                />
-              }
-            />
-            <FormControl.ErrorMessage>
-              {formik.errors.password}
-            </FormControl.ErrorMessage>
-          </FormControl>
-        </Stack>
-        <Button
-          borderRadius="50"
-          size="lg"
-          title="Ingresar"
-          onPress={formik.handleSubmit}>
-          Ingresar
-        </Button>
-        <Text textAlign="center" marginTop="20" color="red.500">
-          {error}
-        </Text>
-      </View>
-    </View>
+    <KeyboardAvoidingView
+      backgroundColor="white"
+      height="100%"
+      h={{
+        base: '100%',
+        lg: 'auto',
+      }}
+      behavior={behavior}
+      enabled>
+      <Box safeArea={true}>
+        <Image
+          source={require('../../assets/account.png')}
+          resizeMode="cover"
+          style={{ width: 400, height: 200 }}
+        />
+        <View paddingX="25">
+          <Text fontSize="26" fontWeight="bold" marginTop="30" marginBottom="8">
+            Iniciar sesión
+          </Text>
+          <Stack space={4} marginBottom="10">
+            <FormControl isInvalid={!!formik.errors.username}>
+              <Input
+                variant="underlined"
+                placeholder="Nombre de usuario"
+                fontSize="14"
+                autoCapitalize="none"
+                value={formik.values.username}
+                onChangeText={(text) => formik.setFieldValue('username', text)}
+                InputLeftElement={
+                  <Icon
+                    as={MaterialIcons}
+                    name="person"
+                    size="md"
+                    mx="3"
+                    color="muted.400"
+                  />
+                }
+              />
+              <FormControl.ErrorMessage>
+                {formik.errors.username}
+              </FormControl.ErrorMessage>
+            </FormControl>
+            <FormControl isInvalid={!!formik.errors.password}>
+              <Input
+                variant="underlined"
+                placeholder="Contraseña"
+                fontSize="14"
+                autoCapitalize="none"
+                secureTextEntry={true}
+                value={formik.values.password}
+                onChangeText={(text) => formik.setFieldValue('password', text)}
+                InputLeftElement={
+                  <Icon
+                    as={MaterialIcons}
+                    name="lock"
+                    size="md"
+                    mx="3"
+                    color="muted.400"
+                  />
+                }
+                type={show ? 'text' : 'password'}
+                InputRightElement={
+                  <Icon
+                    as={MaterialIcons}
+                    name={show ? 'visibility' : 'visibility-off'}
+                    size={5}
+                    mr="2"
+                    color="muted.400"
+                    onPress={() => setShow(!show)}
+                  />
+                }
+              />
+              <FormControl.ErrorMessage>
+                {formik.errors.password}
+              </FormControl.ErrorMessage>
+            </FormControl>
+          </Stack>
+          <Button
+            borderRadius="50"
+            size="lg"
+            title="Ingresar"
+            onPress={formik.handleSubmit}>
+            Ingresar
+          </Button>
+          <Text textAlign="center" marginTop="20" color="red.500">
+            {error}
+          </Text>
+        </View>
+      </Box>
+    </KeyboardAvoidingView>
   );
 };
 
