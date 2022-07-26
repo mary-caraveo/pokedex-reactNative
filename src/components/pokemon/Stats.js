c43fb99c3edb320ef1c153bbc135109aefb34919
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2022-07-26 17:54:44 -0500

    feat: add stats style with native base

--- src/components/pokemon/Stats.js
+++ src/components/pokemon/Stats.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { View, Text } from 'native-base';
 import { map, capitalize } from 'lodash';
 
-export default function Stats(props) {
+const Stats = (props) => {
   const { stats } = props;
 
   const barStyles = (num) => {
-    const color = num > 49 ? "#00ac17" : "#ff3e3e";
+    const color = num > 49 ? '#00ac17' : '#ff3e3e';
     return {
       backgroundColor: color,
       width: `${num}%`,
@@ -14,65 +14,38 @@ export default function Stats(props) {
   };
 
   return (
-    <View style={styles.content}>
-      <Text style={styles.title}>Base Stats</Text>
+    <View paddingX="6" marginTop="10" marginBottom="10">
+      <Text fontWeight="bold" fontSize="20" paddingBottom="2">
+        Base Stats
+      </Text>
       {map(stats, (item, index) => (
-        <View key={index} style={styles.block}>
-          <View style={styles.blockTitle}>
-            <Text style={styles.statName}>{capitalize(item.stat.name)}</Text>
+        <View key={index} flexDirection="row" paddingY="3.2">
+          <View width="30%">
+            <Text fontSize="12" color="#6b6b6b">
+              {capitalize(item.stat.name)}
+            </Text>
           </View>
-          <View style={styles.blockInfo}>
-            <Text style={styles.number}>{item.base_stat}</Text>
-            <View style={styles.bgBar}>
-              <View style={[styles.bar, barStyles(item.base_stat)]} />
+          <View width="70%" flexDirection="row" alignItems="center">
+            <Text width="12%" fontSize="12">
+              {item.base_stat}
+            </Text>
+            <View
+              backgroundColor="#dedede"
+              width="88%"
+              height="1.5"
+              borderRadius="20"
+              overflow="hidden">
+              <View
+                height="1.5"
+                borderRadius="20"
+                style={[barStyles(item.base_stat)]}
+              />
             </View>
           </View>
         </View>
       ))}
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  content: {
-    paddingHorizontal: 20,
-    marginTop: 40,
-    marginBottom: 80,
-  },
-  title: {
-    fontWeight: "bold",
-    fontSize: 20,
-    paddingBottom: 5,
-  },
-  block: {
-    flexDirection: "row",
-    paddingVertical: 5,
-  },
-  blockTitle: {
-    width: "30%",
-  },
-  statName: {
-    fontSize: 12,
-    color: "#6b6b6b",
-  },
-  blockInfo: {
-    width: "70%",
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  number: {
-    width: "12%",
-    fontSize: 12,
-  },
-  bgBar: {
-    backgroundColor: "#dedede",
-    width: "88%",
-    height: 5,
-    borderRadius: 20,
-    overflow: "hidden",
-  },
-  bar: {
-    height: 5,
-    borderRadius: 20,
-  },
-});
+export default Stats;
